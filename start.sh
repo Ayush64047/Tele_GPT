5aefd7551b2bff7faec0d5a1b8c87235e90e317e
Aayush <bhaskarsharma64047@gmail.com>
2023-02-10 09:35:42 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+echo "Cloning Repo...."
+
+git clone https://github.com/Ayush64047/Tele_GPT.git /Tele_GPT
+
+cd /Tele_GPT
+
+pip3 install -r requirements.txt
+
+echo "Starting Bot...."
+
+python3 main.py
